76e00eee6d3a84176eedf8ad2a2425e32b4b9309
Sebastián Estrella <sestrella.me@gmail.com>
2018-11-27 22:40:48 -0500

    Setup axios and adding Posts component

--- frontend/react-javascript/src/App.js
+++ frontend/react-javascript/src/App.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   render() {
+    const client = new Client('http://localhost:3001');
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Posts client={client} />
       </div>
     );
   }
 }
+
+class Client  {
+  constructor(baseURL) {
+    this.axios = axios.create({ baseURL });
+  }
+
+  getPosts() {
+    return this.axios.get('posts?_embed=comments');
+  }
+}
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { posts: [] };
+  }
+
+  componentDidMount() {
+    this.props.client.getPosts().then(({ data }) => {
+      this.setState({ posts: data });
+    });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.posts.map(post => <Post key={post.id} post={post} />)}
+      </ul>
+    );
+  }
+}
+
+const Post = ({ post }) => (
+  <li>{post.title} - ({post.comments.length}) comments</li>
+)
 
 export default App;
