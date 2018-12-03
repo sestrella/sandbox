c6c742a4395b09d21d0ba514c21f5e247f6e98f4
Sebastián Estrella <sestrella.me@gmail.com>
2018-12-03 00:33:27 -0500

    [webapi-csharp] Scaffold dbcontext

---
+++ backend/webapi-csharp/webapi_csharpContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace webapi-csharp
+{
+    public partial class webapi_csharpContext : DbContext
+    {
+        public webapi_csharpContext()
+        {
+        }
+
+        public webapi_csharpContext(DbContextOptions<webapi_csharpContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("Host=localhost;Database=webapi_csharp;Username=sestrella;Password=sestrella");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
